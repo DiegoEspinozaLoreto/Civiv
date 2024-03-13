58cb9c5aa88753321a32adb75ad0e91e6b25934e
DMM <mthdewlover16@hotmail.com>
2024-03-12 23:00:15 -0600

    Añadido video y clase de la calculadora

--- app/src/main/java/com/example/civiv/Home.java
+++ app/src/main/java/com/example/civiv/Home.java
@@ -3,13 +3,13 @@ package com.example.civiv;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
         public ImageButton b;
+        public ImageButton calc;
         @Override
 
         protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +18,25 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
 
             b = (ImageButton) findViewById(R.id.regresarMenuButton);
             b.setOnClickListener(this);
+            calc = (ImageButton) findViewById(R.id.btn_calc1);
+            calc.setOnClickListener(this);
+
 
 
     }
 
     @Override
     public void onClick(View v) {
-        Intent transicion = new Intent(Home.this, Login.class);
-        startActivity(transicion);
+
+            if (v.getId()== R.id.regresarMenuButton){
+                Intent transicion = new Intent(Home.this, Login.class);
+                startActivity(transicion);
+            }else {
+                Intent transicion_calc = new Intent(Home.this, calculadora.class);
+
+                startActivity(transicion_calc);
+            }
+
+
     }
 }
